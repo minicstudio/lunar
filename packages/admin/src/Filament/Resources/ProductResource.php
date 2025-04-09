minicstudio/lunar

commit 78e08877e2d40f40b4e969601f458a46836b7395

Add variant attributes to product edit page (#2054)
Fixes #1996

---------

Co-authored-by: Author <actions@github.com>
Co-authored-by: Glenn Jacobs <glenn@neondigital.co.uk>

packages/admin/src/Filament/Resources/ProductResource.php
@@ -123,6 +123,7 @@ class ProductResource extends BaseResource
                         static::getMainFormComponents(),
                     ),
                 static::getAttributeDataFormComponent(),
+                static::getVariantAttributeDataFormComponent(),
             ])
             ->columns(1);
     }
@@ -225,7 +226,14 @@ class ProductResource extends BaseResource
 
     protected static function getAttributeDataFormComponent(): Component
     {
-        return Attributes::make()->statePath('attribute_data');
+        return Attributes::make();
+    }
+
+    protected static function getVariantAttributeDataFormComponent(): Component
+    {
+        return Attributes::make()
+            ->using(ProductVariant::class)
+            ->relationship('variant');
     }
 
     public static function getDefaultTable(Table $table): Table

packages/admin/src/Support/Forms/Components/Attributes.php
@@ -14,15 +14,24 @@ use Lunar\Models\ProductVariant;
 
 class Attributes extends Forms\Components\Group
 {
+    public ?string $modelClassOverride = null;
+
+    public function using(string $modelClass): self
+    {
+        $this->modelClassOverride = $modelClass;
+
+        return $this;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->key('attributeData');
+        $this->key('attributeData'.$this->modelClassOverride);
 
         if (blank($this->childComponents)) {
             $this->schema(function (\Filament\Forms\Get $get, Livewire $livewire, ?Model $record) {
-                $modelClass = $livewire::getResource()::getModel();
+                $modelClass = $this->modelClassOverride ?: $livewire::getResource()::getModel();
 
                 $productTypeId = null;
 
@@ -42,7 +51,12 @@ class Attributes extends Forms\Components\Group
                 }
 
                 if ($morphMap == ProductVariant::morphName()) {
-                    $productTypeId = $record?->product?->product_type_id ?: ProductType::first()->id;
+                    if ($record::class === Product::modelClass()) {
+                        $productTypeId = $record?->product_type_id ?: ProductType::first()->id;
+                    } else {
+                        $productTypeId = $record?->product?->product_type_id ?: ProductType::first()->id;
+                    }
+
                     // If we have a product type, the attributes should be based off that.
                     if ($productTypeId) {
                         $attributeQuery = ProductType::find($productTypeId)->variantAttributes();
@@ -72,9 +86,9 @@ class Attributes extends Forms\Components\Group
                     foreach ($group['fields'] as $field) {
                         $sectionFields[] = AttributeData::getFilamentComponent($field);
                     }
-
                     $groupComponents[] = Forms\Components\Section::make($group['model']->translate('name'))
-                        ->schema($sectionFields);
+                        ->schema($sectionFields)
+                        ->statePath('attribute_data');
                 }
 
                 return $groupComponents;

packages/core/src/Models/Product.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Lunar\Base\BaseModel;
@@ -110,6 +111,11 @@ class Product extends BaseModel implements Contracts\Product, SpatieHasMedia
     public function variants(): HasMany
     {
         return $this->hasMany(ProductVariant::modelClass());
+    }
+
+    public function variant(): HasOne
+    {
+        return $this->hasOne(ProductVariant::modelClass());
     }
 
     public function collections(): BelongsToMany

tests/admin/Feature/Filament/Resources/ProductResource/Pages/EditProductTest.php
@@ -1,0 +1,64 @@
+<?php
+
+use Livewire\Livewire;
+
+uses(\Lunar\Tests\Admin\Unit\Filament\TestCase::class)
+    ->group('resource.product');
+
+it('can edit variant attributes', function () {
+    \Lunar\Models\CustomerGroup::factory()->create([
+        'default' => true,
+    ]);
+
+    \Lunar\Models\Language::factory()->create([
+        'default' => true,
+    ]);
+
+    $product = \Lunar\Models\Product::factory()->create();
+    $variant = \Lunar\Models\ProductVariant::factory()->create([
+        'product_id' => $product->id,
+    ]);
+
+    $group = \Lunar\Models\AttributeGroup::factory()->create([
+        'attributable_type' => 'product_variant',
+        'name' => [
+            'en' => 'Variant Details',
+        ],
+        'handle' => 'variant_details',
+        'position' => 1,
+    ]);
+
+    $attribute = \Lunar\Models\Attribute::factory()->create([
+        'attribute_type' => 'product_variant',
+        'attribute_group_id' => $group->id,
+        'position' => 1,
+        'name' => [
+            'en' => 'Test Attribute',
+        ],
+        'handle' => 'test-attribute',
+        'section' => 'main',
+        'required' => false,
+        'system' => false,
+        'searchable' => false,
+    ]);
+
+    \Illuminate\Support\Facades\DB::table('lunar_attributables')->insert([
+        'attribute_id' => $attribute->id,
+        'attributable_type' => 'product_type',
+        'attributable_id' => $product->productType->id,
+    ]);
+
+    $this->asStaff(admin: true);
+
+    $component = Livewire::test(\Lunar\Admin\Filament\Resources\ProductResource\Pages\EditProduct::class, [
+        'record' => $product->id,
+        'pageClass' => 'productEdit',
+    ])->assertSuccessful();
+
+    expect($variant->attr($attribute->handle))->toBeNull();
+
+    $component->set('data.variant.attribute_data.'.$attribute->handle, 'Hello');
+    $component->call('save');
+
+    expect($variant->refresh()->attr($attribute->handle))->toBe('Hello');
+});

commit 899e4f469d3de175e13cd4557e6c6ec26ba3ad11

Fix saving product single variant (#2159)
fixes bug introduced in #2054

packages/admin/src/Filament/Resources/ProductResource.php
@@ -233,7 +233,8 @@ class ProductResource extends BaseResource
     {
         return Attributes::make()
             ->using(ProductVariant::class)
-            ->relationship('variant');
+            ->relationship('variant')
+            ->hidden(fn (Product $record) => $record->hasVariants);
     }
 
     public static function getDefaultTable(Table $table): Table

packages/admin/src/Filament/Resources/ProductVariantResource.php
@@ -296,7 +296,7 @@ class ProductVariantResource extends BaseResource
 
     protected static function getAttributeDataFormComponent(): Component
     {
-        return Attributes::make()->statePath('attribute_data');
+        return Attributes::make();
     }
 
     public static function getDefaultTable(Table $table): Table

packages/admin/src/Support/Forms/Components/Attributes.php
@@ -2,6 +2,7 @@
 
 namespace Lunar\Admin\Support\Forms\Components;
 
+use Closure;
 use Filament\Forms;
 use Illuminate\Database\Eloquent\Model;
 use Livewire\Component as Livewire;
@@ -16,6 +17,24 @@ class Attributes extends Forms\Components\Group
 {
     public ?string $modelClassOverride = null;
 
+    protected string|Closure $attributeDataField = 'attribute_data';
+
+    public function attributeDataField(string|Closure $attributeDataField): static
+    {
+        $this->attributeDataField = $attributeDataField;
+
+        if (blank($this->relationship)) {
+            $this->statePath($attributeDataField);
+        }
+
+        return $this;
+    }
+
+    public function getAttributeDataField(): string
+    {
+        return $this->evaluate($this->attributeDataField);
+    }
+
     public function using(string $modelClass): self
     {
         $this->modelClassOverride = $modelClass;
@@ -23,11 +42,16 @@ class Attributes extends Forms\Components\Group
         return $this;
     }
 
+    public function getKey(): ?string
+    {
+        return 'attributeData'.$this->modelClassOverride;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->key('attributeData'.$this->modelClassOverride);
+        $this->statePath('attribute_data');
 
         if (blank($this->childComponents)) {
             $this->schema(function (\Filament\Forms\Get $get, Livewire $livewire, ?Model $record) {
@@ -86,9 +110,9 @@ class Attributes extends Forms\Components\Group
                     foreach ($group['fields'] as $field) {
                         $sectionFields[] = AttributeData::getFilamentComponent($field);
                     }
-                    $groupComponents[] = Forms\Components\Section::make($group['model']->translate('name'))
-                        ->schema($sectionFields)
-                        ->statePath('attribute_data');
+                    $groupComponents[] = Forms\Components\Section::make($group['model']
+                        ->translate('name'))
+                        ->schema($sectionFields);
                 }
 
                 return $groupComponents;
@@ -109,6 +133,16 @@ class Attributes extends Forms\Components\Group
             }
 
             return $state;
+        });
+
+        $this->mutateRelationshipDataBeforeSaveUsing(static function (Attributes $component, array $data): array {
+            return [
+                $component->getAttributeDataField() => $data,
+            ];
+        });
+
+        $this->mutateRelationshipDataBeforeFillUsing(static function (Attributes $component, array $data): array {
+            return $data[$component->getAttributeDataField()] ?? [];
         });
     }
 }

packages/core/src/Models/Contracts/Product.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 interface Product
@@ -31,6 +32,11 @@ interface Product
      * Return the product variants relation.
      */
     public function variants(): HasMany;
+
+    /**
+     * Return the product variant relation.
+     */
+    public function variant(): HasOne;
 
     /**
      * Return the product collections relation.

packages/core/src/Models/Product.php
@@ -118,6 +118,13 @@ class Product extends BaseModel implements Contracts\Product, SpatieHasMedia
         return $this->hasOne(ProductVariant::modelClass());
     }
 
+    protected function hasVariants(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->variants()->count() > 1,
+        );
+    }
+
     public function collections(): BelongsToMany
     {
         return $this->belongsToMany(

tests/admin/Feature/Filament/Resources/ProductResource/Pages/EditProductTest.php
@@ -57,8 +57,12 @@ it('can edit variant attributes', function () {
 
     expect($variant->attr($attribute->handle))->toBeNull();
 
-    $component->set('data.variant.attribute_data.'.$attribute->handle, 'Hello');
-    $component->call('save');
+    $component->fillForm([
+        'variant' => [
+            $attribute->handle => new \Lunar\FieldTypes\Text('Hello'),
+        ],
+    ])->call('save')
+        ->assertHasNoFormErrors();
 
     expect($variant->refresh()->attr($attribute->handle))->toBe('Hello');
 });