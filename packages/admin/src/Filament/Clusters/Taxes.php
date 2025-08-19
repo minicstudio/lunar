1346a839e9310f23af24d763712ab9ef5921debc
Lehel <lehel.vajda@minic.ro>
2025-08-19 10:53:21 +0300

    delete duplicate methods

--- packages/admin/src/Filament/Clusters/Taxes.php
+++ packages/admin/src/Filament/Clusters/Taxes.php
@@ -19,19 +19,9 @@ class Taxes extends Cluster
         return __('lunarpanel::tax.plural_label');
     }
 
-    public static function getClusterBreadcrumb(): ?string
-    {
-        return __('lunarpanel::tax.plural_label');
-    }
-
     public static function getNavigationIcon(): ?string
     {
         return FilamentIcon::resolve('lunar::tax');
-    }
-
-    public static function getNavigationLabel(): string
-    {
-        return __('lunarpanel::tax.plural_label');
     }
 
     public static function getClusterBreadcrumb(): string
