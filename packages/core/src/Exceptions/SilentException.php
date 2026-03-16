2f87dbc08ff42656ab61ab3934fdb71f51082c41
Lehel <lehel.vajda@minic.ro>
2026-03-16 13:18:28 +0200

    chore: add documentation to SilentException class

--- packages/core/src/Exceptions/SilentException.php
+++ packages/core/src/Exceptions/SilentException.php
@@ -6,5 +6,6 @@ use Exception;
 
 class SilentException extends Exception
 {
-    //
+    // This exception is used to silently fail certain operations without displaying errors to the user,
+    // but sending error reports to monitoring services like Nightwatch
 }
