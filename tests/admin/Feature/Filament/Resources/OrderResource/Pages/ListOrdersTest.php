8a0ad60ba331bbc8fcd26366c5a767b9d5ba494b
Vajda Lehel <lehel.vajda@minic.ro>
2026-08-28 11:20:25 +0300

    test: LFP-783 fix order list tests for default draft-order filter

--- tests/admin/Feature/Filament/Resources/OrderResource/Pages/ListOrdersTest.php
+++ tests/admin/Feature/Filament/Resources/OrderResource/Pages/ListOrdersTest.php
@@ -2,10 +2,17 @@
 
 use Livewire\Livewire;
 use Lunar\Admin\Filament\Resources\OrderResource\Pages\ListOrders;
+use Lunar\Models\Currency;
 use Lunar\Models\Order;
 
 uses(\Lunar\Tests\Admin\Feature\Filament\TestCase::class)
     ->group('resource.order');
+
+beforeEach(function () {
+    Currency::factory()->create([
+        'default' => true,
+    ]);
+});
 
 it('excludes orders that have not been placed by default', function () {
     $this->asStaff();
@@ -14,6 +21,7 @@ it('excludes orders that have not been placed by default', function () {
     $draft = Order::factory()->create(['placed_at' => null]);
 
     Livewire::test(ListOrders::class)
+        ->call('loadTable')
         ->assertCanSeeTableRecords([$placed])
         ->assertCanNotSeeTableRecords([$draft]);
 });
@@ -25,6 +33,7 @@ it('includes not yet placed orders when show_draft_orders is enabled and no date
     $draft = Order::factory()->create(['placed_at' => null]);
 
     Livewire::test(ListOrders::class)
+        ->call('loadTable')
         ->filterTable('placed_at', [
             'placed_after' => null,
             'placed_before' => null,
@@ -40,6 +49,7 @@ it('ignores show_draft_orders when a placed date range is set', function () {
     $draft = Order::factory()->create(['placed_at' => null]);
 
     Livewire::test(ListOrders::class)
+        ->call('loadTable')
         ->filterTable('placed_at', [
             'placed_after' => now()->subDay()->toDateString(),
             'placed_before' => null,
